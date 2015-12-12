b721e0a19d15bb83bdcffc97b4a49a99ec8a931e
gwq <457781132@qq.com>
2015-12-12 23:11:01 +0800

    Create CpuInfo.py

---
+++ CpuInfo.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+class CpuInfo:
+    def __init__(self, filename = "/proc/stat"):
+        self.info = {};
+        self.filename = filename;
+        self.usageKey = "usage";
+        self.LoadCpuInfo();
+        self.CalcUsage();
+
+    def GetCoreNum(self):
+        return len(self.info) - 1;
+
+    def LoadCpuInfo(self):
+        with open(self.filename) as f:
+            for line in f:
+                coreInfo = {};
+                datas = line.split();
+                name = datas[0];
+                if name.find("cpu") < 0:
+                    continue;
+                coreInfo["user"] = int(datas[1]);
+                coreInfo["nice"] = int(datas[2]);
+                coreInfo["system"] = int(datas[3]);
+                coreInfo["idle"] = int(datas[4]);
+                coreInfo["iowait"] = int(datas[5]);
+                coreInfo["irq"] = int(datas[6]);
+                coreInfo["softirq"] = int(datas[7]);
+                self.info[name] = coreInfo;
+
+    def GetCoreInfo(self, idx):
+        key = "cpu%d" % idx;
+        return self.info[key];
+
+    def GetCoreUsage(self, idx):
+        return self.GetCoreInfo(idx)[self.usageKey];
+
+    def GetAveUsage(self, idx):
+        return self.GetAveInfo()[self.usageKey];
+
+    def GetMaxUsage(self):
+        idx = -1;
+        maxUsage = 0.0;
+        coreNum = self.GetCoreNum();
+        for num in range(coreNum):
+            key = "cpu%d" % num;
+            coreInfo = self.info[key];
+            if coreInfo[self.usageKey] > maxUsage:
+                maxUsage = coreInfo[self.usageKey];
+                idx = num;
+        return maxUsage, idx
+
+    def GetAvgInfo(self):
+        key = "cpu";
+        return self.info[key];
+
+    def CalcUsage(self):
+        for key in self.info:
+            coreInfo = self.info[key];
+            total = 0;
+            for coreKey in coreInfo:
+                total += coreInfo[coreKey];
+            used = total - coreInfo["iowait"] - coreInfo["idle"];
+            if total == 0:
+                coreInfo[self.usageKey] = 0;
+            else:
+                coreInfo[self.usageKey] = used * 100.0 / total;
+
+if __name__ == "__main__":
+    cpuInfo = CpuInfo("stat");
+    print "CoreNum %d." % cpuInfo.GetCoreNum();
+    print "Core %d Info:\n%s" % (1, cpuInfo.GetCoreInfo(1));
+    print "Ave Info:"
+    print cpuInfo.GetAvgInfo();
+
+    maxUsage, maxIdx, coreNum = cpuInfo.GetMaxUsage();
+    print "maxUsage: %.2f" % maxUsage;
+    print "maxIdx: %d" % maxIdx;
+    print "CoreNum: %d" % coreNum;
