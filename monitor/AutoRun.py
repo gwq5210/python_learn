098da9b7e6ec75635ded7145f5628701dd91f665
gwq <gwq5210@qq.com>
2015-12-18 10:54:28 +0800

    1, modify: AutoRun.py

--- monitor/AutoRun.py
+++ monitor/AutoRun.py
@@ -2,7 +2,58 @@
 # coding=utf-8
 
 import os;
+import sys;
+import time;
 from Utils import *;
+
+stdout = "/dev/null"
+stdin = "/dev/null"
+stderr = None
+pidfile = "./.daemon.pid"
+if pidfile:
+    print("pid file alread exist!")
+sys.stdout.flush()
+sys.stdin.flush()
+try:
+    pid = os.fork()
+    if pid > 0:
+        sys.exit() #父进程退出只剩下子进程
+except OSError,e:
+    sys.stderr.write("fork #1 failed:(%d)%s\n" % (e.errno,e.strerror))
+    sys.exit()
+#调用‘chdir("/")’确认我们的进程不保持任何目录于使用状态。不做这个会导致系统管理员不能卸装(umount)一个文件系统，因为它是我们的当前工作目录。 [类似的，我们可以改变当前目录至对于守护程序运行重要的文件所在目录]
+#改变当前进程运行目录 
+os.chdir("/")
+#使当前线程 拥有文件读写权限
+os.umask(0)
+#使子线程 脱离父线程的进程组，会话组，是子进程成为新的的头领， 进程同时与会话脱离 ，
+os.setsid()
+try:
+    pid = os.fork() #父进程的会话头领可以退出，以为着非会话头领进程永远不可能获得终端
+    if pid > 0 :
+        sys.exit()
+except OSError,e:
+    sys.stderr.write("fork #2 failed:(%d)%s\n"  % (e.errno,e.strerror))
+#调用close关闭从父进程继承过来的标准输入输出
+if not stderr:
+    stderr = stdout
+    si = file(stdin,'r')
+    so = file(stdout,'a+')
+    se = file(stderr,'a+',0)#ubuffered
+    pid = str(os.getpid())
+    print("daemon process id:%s" % pid)
+    sys.stderr.write("\n%s\n" % pid)
+    sys.stderr.flush()
+    if pidfile:
+        abspath = os.path.abspath(pidfile)
+        print(abspath)
+        print("write to pid file :true")
+        open(pidfile,"a").write("%s\n" % pid)
+   #改变文件符号
+    #os.dup2(si.fileno(), sys.stdin.fileno())
+    #os.dup2(so.fileno(),sys.stdout.fileno())  
+    #os.dup2(se.fileno(),sys.stderr.fileno())  
+
 
 def SetLSvrMcpCnt(mcp_cnt):
     ip = "10.175.70.10";
